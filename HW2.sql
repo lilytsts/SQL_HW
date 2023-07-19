e471e886d34e3898fccff327eda9eead78f26810
Liliya Tsikul <116584197+lilytsts@users.noreply.github.com>
2023-07-19 19:35:03 +0300

    Update HW2.sql

--- HW2.sql
+++ HW2.sql
@@ -1,7 +1,6 @@
 -- 1.Создать таблицу employees
 -- - id. serial,  primary key,
 -- - employee_name. Varchar(50), not null
-
 create table employees(
 	id serial primary key,
 	employee_name varchar(50) not null
